353443f7556c427b74c2e2a2ced92d9e5ecee4ea
gablaureano <gabrielkoanuilaureano@yahoo.com>
2024-08-16 18:55:41 -0400

    added task 4 getting the book details based on the title and task 5 getting the book reviews

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -41,14 +41,25 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const title = req.params.title;
+    let bookKeys = Object.keys(books);
+    let filtered_keys = bookKeys.filter((key) => books[key].title === title);
+    let filtered_books = [];
+    filtered_keys.forEach((key) => filtered_books.push(books[key]));
+
+    if (filtered_books.length > 0) {
+        res.send(filtered_books);
+    } else {
+        res.send(`No books with title: ${title}.`);
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
