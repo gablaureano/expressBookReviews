b877b6d3ea92f9fa8c672aa07d1c1758b76c812a
gablaureano <gabrielkoanuilaureano@yahoo.com>
2024-08-17 18:19:36 -0400

    added task 11-13 adding promise function into general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,46 +28,78 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
     //Write your code here
-    res.send(JSON.stringify({books}, null, 4));
+    const getBookList = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+    });
+    getBookList
+        .then(() => console.log("This is for task 10"));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     //Write your code here
-    const isbn = req.params.isbn;
-    res.send(books[isbn]);
+    const getBookList = new Promise((resolve, reject) => {
+        try {
+            const isbn = req.params.isbn;
+            resolve(res.send(books[isbn]));
+        } catch (err) {
+            reject(err);
+        }
+    });
+    getBookList
+        .then(() => console.log("This is for task 11"));
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     //Write your code here
     const author = req.params.author;
-    let bookKeys = Object.keys(books);
-    let filtered_keys = bookKeys.filter((key) => books[key].author === author);
-    let filtered_books = [];
-    filtered_keys.forEach((key) => filtered_books.push(books[key]));
+    const getBookList = new Promise((resolve, reject) => {
+        try {
+            let bookKeys = Object.keys(books);
+            let filtered_keys = bookKeys.filter((key) => books[key].author === author);
+            let filtered_books = [];
+            filtered_keys.forEach((key) => filtered_books.push(books[key]));
+            resolve(filtered_books);
+        } catch (err) {
+            reject(err);
+        }
+    });
 
-    if (filtered_books.length > 0) {
-        res.send(filtered_books);
-    } else {
-        res.send(`No books authored by: ${author}.`);
-    }
+    getBookList
+        .then((filtered_books) => {
+            if (filtered_books.length > 0) {
+                res.send(filtered_books);
+            } else {
+                res.send(`No books authored by: ${author}.`);
+            }
+        });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     //Write your code here
     const title = req.params.title;
-    let bookKeys = Object.keys(books);
-    let filtered_keys = bookKeys.filter((key) => books[key].title === title);
-    let filtered_books = [];
-    filtered_keys.forEach((key) => filtered_books.push(books[key]));
-
-    if (filtered_books.length > 0) {
-        res.send(filtered_books);
-    } else {
-        res.send(`No books with title: ${title}.`);
-    }
+    const getBookList = new Promise((resolve, reject) => {
+        try {
+            let bookKeys = Object.keys(books);
+            let filtered_keys = bookKeys.filter((key) => books[key].title === title);
+            let filtered_books = [];
+            filtered_keys.forEach((key) => filtered_books.push(books[key]));
+            resolve(filtered_books);
+        } catch (err) {
+            reject(err);
+        }
+    });
+    
+    getBookList
+        .then( (filtered_books) => {
+            if (filtered_books.length > 0) {
+                res.send(filtered_books);
+            } else {
+                res.send(`No books with title: ${title}.`);
+            }
+        });
 });
 
 //  Get book review
